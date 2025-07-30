983f28bb4539847a727b434b4fdff6861c98fe7c
Prajwalkr-789 <prajwalkrpkrp@gmail.com>
2025-07-30 23:58:53 +0530

    First commit

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ const app = express();
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  credentials: true
+  origin: '*',
 }));
 
 // Body parsing middleware
